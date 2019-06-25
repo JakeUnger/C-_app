07debfcb5b9b4d95b0bbb73f535a68e1ae0fb4df
Jake2019 <jakeu34@gmail.com>
2019-06-25 16:32:22 -0400

    Create Progam.cs initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+          {
+                Console.WriteLine(CompoundInterest(5000, 0.0251, 1, 1));
+            }
+
+            static double CompoundInterest(double principal, double interestRate, int timesPerYear, double years)
+            {
+
+                double body = 1 + (interestRate / timesPerYear);
+
+                double exponent = timesPerYear * years;
+
+                return principal * Math.Pow(body, exponent);
+
+
+
+
+            }
+        }
+}
